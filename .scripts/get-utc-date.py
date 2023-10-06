fa1a32dc68c09ef545fe5c4eba1707f62bd1fb20
smordarski <steve@mordarski.net>
2023-10-05 20:38:31 -0400

    October Day 5 🍂☕: + Added .scripts directory with get_utc_date Python 3 script + Found more bizarre TikTok & TikTok Downloader websites + Updated hosts.txt (Both) + Added a requested Discord bot website to the filter list

---
+++ .scripts/get-utc-date.py
@@ -0,0 +1,27 @@
+# MIT License
+#
+# Copyright (c) 2023 Steven Mordarski
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+import os
+import subprocess
+from datetime import datetime, timezone
+
+utc_time = datetime.now(timezone.utc).strftime('%A, %B %d, %Y %I:%M:%S %p %Z')
+
+# Copy utc_time to the clipboard
+if os.name == 'nt':
+    subprocess.run("clip", check=False, shell=True, input=utc_time.encode())
+    print("Copied UTC time to the clipboard.")
+elif os.name == 'posix':
+    subprocess.run("xclip", check=False, shell=True, input=utc_time.encode())
+    print("Copied UTC time to the clipboard.")
