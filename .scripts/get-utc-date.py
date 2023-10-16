c0eb940be7724c7b8a8c69cfa4866e73c1983ee0
smordarski <steve@mordarski.net>
2023-10-16 17:31:17 -0400

    Replace os with platform module in get-utc-date.py

--- .scripts/get-utc-date.py
+++ .scripts/get-utc-date.py
@@ -1,7 +1,7 @@
 # MIT License
 #
 # Copyright (c) 2023 Mordarski Networks
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
@@ -12,16 +12,21 @@
 # The above copyright notice and this permission notice shall be included in all
 # copies or substantial portions of the Software.
 
-import os
+import platform
 import subprocess
 from datetime import datetime, timezone
 
 utc_time = datetime.now(timezone.utc).strftime('%A, %B %d, %Y %I:%M:%S %p %Z')
 
 # Copy utc_time to the clipboard
-if os.name == 'nt':
+if platform.system() == "Windows":
     subprocess.run("clip", check=False, shell=True, input=utc_time.encode())
     print("Copied UTC time to the clipboard.")
-elif os.name == 'posix':
+elif platform.system() == "Linux":
     subprocess.run("xclip", check=False, shell=True, input=utc_time.encode())
     print("Copied UTC time to the clipboard.")
+elif platform.system() == "Darwin":
+    subprocess.run("pbcopy", check=False, shell=True, input=utc_time.encode())
+    print("Copied UTC time to the clipboard.")
+else:
+    print("This OS is not supported.")
